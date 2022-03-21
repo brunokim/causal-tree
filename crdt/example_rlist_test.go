6b42cf46247e66b689d9bb9cf8edbe261efcdeb7
Bruno Kim <brunokim.mc@gmail.com>
2022-03-21 00:37:13 -0300

    Include example of ViewAt

--- crdt/example_rlist_test.go
+++ crdt/example_rlist_test.go
@@ -65,3 +65,39 @@ func ExampleRList_overlap() {
 	fmt.Println(l1.AsString())
 	// Output: dreser
 }
+
+//
+func ExampleRList_ViewAt() {
+	s0 := crdt.NewRList()    // S0 @ T1
+	s0.InsertChar('a')       // S0 @ T2
+	s1, _ := s0.Fork()       // S0 @ T3
+	s1.InsertChar('b')       // S1 @ T4
+	s1.InsertChar('c')       // S1 @ T5
+	s2, _ := s0.Fork()       // S0 @ T4
+	s2.InsertCharAt('x', -1) // S2 @ T5
+	s2.InsertChar('y')       // S2 @ T6
+	s2.Merge(s1)             // S2 @ T7
+
+	// Now s2 reads as "xyabc", with each char having the following IDs and causes:
+	// x: s2 @ T5, caused by the zero atom
+	// y: s2 @ T6, caused by s2 @ T5 (x)
+	// a: s0 @ T2, caused by the zero atom
+	// b: s1 @ T4, caused by s0 @ T2 (a)
+	// c: s1 @ T5, caused by s1 @ T4 (b)
+
+	// Timestamps for each site: s0=4, s1=5, s2=7
+	v1, _ := s2.ViewAt(crdt.Weft{4, 5, 7})
+	v2, _ := s2.ViewAt(crdt.Weft{4, 5, 0})
+	v3, _ := s2.ViewAt(crdt.Weft{4, 0, 7})
+	v4, _ := s2.ViewAt(crdt.Weft{0, 3, 7}) // With s0=0, we need to cut s1 down to T3, because it is ultimately caused by 'a' from s0.
+
+	fmt.Println("Now: ", v1.AsString())
+	fmt.Println("s2=0:", v2.AsString())
+	fmt.Println("s1=0:", v3.AsString())
+	fmt.Println("s0=0:", v4.AsString())
+	// Output:
+	// Now:  xyabc
+	// s2=0: abc
+	// s1=0: xya
+	// s0=0: xy
+}
